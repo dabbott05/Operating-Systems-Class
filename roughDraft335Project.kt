5126ccbf1cbadc3198ecbd4850720acc866961ad
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2025-09-24 10:17:11 -0500

    Updated comments

--- roughDraft335Project.kt
+++ roughDraft335Project.kt
@@ -8,22 +8,22 @@ class Worker(val name: String) : Runnable {
 fun main() {
     println("======NO SYNC======") // header
     // create 5 threads and have them execute the run() function
-    val tasks = List(5) {thread {Worker("Task $it").run()}}
-    tasks.forEach {it.join()} // for each thread .join()
+    val tasks = List(5) {thread {Worker("Task ${it + 1}").run()}} // no index defined so we use $it
+    tasks.forEach {it.join()} // for each thread .join(). we use $it because no index was defined
 
     println("======SYNC======")
     val lock = ReentrantLock() // instance of ReentrantLock class. provides a mutual exclusion lock allowing threads to lock and unlock
     val condition = lock.newCondition() // Condition object. allows threads to wait for a specific condition to be met before continuing
     var count = 0 // shared counter to check which threads turn it is
-
-    val syncTasks = List(5) { i -> // create lambda function. i is used for task order. each iteration of i executes the lambda function
-        thread{ // threads will run concurrently, not at the same time but in the same period of time
+    val syncTasks = List(5) { i -> // create lambda function. i is used for task order. each iteration of i executes the lambda function. we dont use it since we defined i
+        thread { // creates a thread that will run the specified code
             lock.lock() // acquires the lock. ensures each thread executes the following blocks one at a time since count and condition cant be executed concurrently
             try { // if something goes wrong we need the lock to unlock in the finally clause
                 while (count != i) { // the thread checks to see if it is its turn to run
                     condition.await() // .await(), "Causes the current thread to wait until it is signalled or interrupted." https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/Condition.html
                 }
-                Worker("Task $i").run() // creates and runs an instance of the Worker class with the name Task $i
+                //println(i is Int)
+                Worker("Task ${i+1}").run() // creates and runs an instance of the Worker class with the name Task $i
                 count++ // increment so that the next thread in line can proceed
                 condition.signalAll() //  wake up all the threads that were hit with the .await() method
             }
@@ -34,14 +34,16 @@ fun main() {
 
     }
     syncTasks.forEach() {it.join()} // wait for all threads to finish before proceeding. .join() - blocks the main thread until each worker thread completes
+    // we need to use $it since no index is defined in the lambda
 
     println("======Priorities======")
-    val pTasks = List(5) { i -> // create 5 threads and a lambda function with code for each thread to execute
+    val pTasks = List(5) { i: Int -> // create 5 threads and a lambda function with code for each thread to execute. since i is defined we dont use $it
         val t = thread(start = false) { // we do not start the threads right away
             println("Thread ${i+1} priority: ${Thread.currentThread().priority}") // print the current thread and its priority
-            repeat(2) { // repeat this function twice
-                println("Thread ${i+1}: Step ${it+1}") // i is used for our thread task count and it is used to show what iteration of the repeat() function we are one
-                Thread.sleep(10)  // small delay
+            Thread.sleep(100) // gives ordered print before running the repeat() {} block
+            repeat(2) { // repeat the lambda twice. since no parameter is defined $it is implicity used as index for the repeat() function
+                println("Thread ${i+1}: Step ${it+1}") // i is used for our thread task count and it is used to show what index of the repeat() function we are one
+                Thread.sleep(100)  // small delay
             }
         }
         // most modern operating system schedulers ignore priorities since they think they know better
@@ -49,7 +51,7 @@ fun main() {
             t.priority = 2  //4
         }
         else if (i == 1) {
-            t.priority = 1 //5
+            t.priority = Thread.MIN_PRIORITY //5
         }
         else if (i == 2) {
             t.priority = 9 //2
@@ -66,8 +68,12 @@ fun main() {
         t  // return thread object so pTasks will be a list of them
     }
     pTasks.forEach { it.start() }  // start all
-    pTasks.forEach { it.join() }   // wait for completion
-
+    pTasks.forEach { it.join() }   // wait for completion before exiting
+    // no index is defined so we need to use $it
     }
+
+
+
+
 
 
