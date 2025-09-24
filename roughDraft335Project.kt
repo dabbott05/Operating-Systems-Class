6aa2974f3c7e75069575d9add506a902ecdd7fc7
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2025-09-24 12:12:16 -0500

    updated comments and changed variable names

--- roughDraft335Project.kt
+++ roughDraft335Project.kt
@@ -1,34 +1,33 @@
-import kotlin.concurrent.thread
-import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.thread // unlocks threads
+import java.util.concurrent.locks.ReentrantLock //allows us to use locks and conditions
 
-class Worker(val name: String) : Runnable {
+class Worker(val name: String) : Runnable { // The Runnable interface should be implemented by any class whose instances are intended to be executed by a thread
     override fun run() = println("$name running") // simple task for the threads to print
 }
 
 fun main() {
     println("======NO SYNC======") // header
     // create 5 threads and have them execute the run() function
-    val tasks = List(5) {thread {Worker("Task ${it + 1}").run()}} // no index defined so we use $it
-    tasks.forEach {it.join()} // for each thread .join(). we use $it because no index was defined
+    val unsyncTasks = List(5) { thread {Worker("Task ${it + 1}").run()}} // no index defined so we use $it
+    unsyncTasks.forEach {it.join()} // for each thread .join(). we use $it because no index was defined
 
     println("======SYNC======")
-    val lock = ReentrantLock() // instance of ReentrantLock class. provides a mutual exclusion lock allowing threads to lock and unlock
-    val condition = lock.newCondition() // Condition object. allows threads to wait for a specific condition to be met before continuing
+    val access = ReentrantLock() // instance of ReentrantLock class. provides a mutual exclusion lock allowing threads to lock and unlock
+    val condition = access.newCondition() // Condition object. allows threads to wait for a specific condition to be met before continuing
     var count = 0 // shared counter to check which threads turn it is
     val syncTasks = List(5) { i -> // create lambda function. i is used for task order. each iteration of i executes the lambda function. we dont use it since we defined i
         thread { // creates a thread that will run the specified code
-            lock.lock() // acquires the lock. ensures each thread executes the following blocks one at a time since count and condition cant be executed concurrently
+            access.lock() // acquires the lock. ensures each thread executes the following blocks one at a time since count and condition cant be executed concurrently. increases hold count
             try { // if something goes wrong we need the lock to unlock in the finally clause
                 while (count != i) { // the thread checks to see if it is its turn to run
                     condition.await() // .await(), "Causes the current thread to wait until it is signalled or interrupted." https://docs.oracle.com/javase/7/docs/api/java/util/concurrent/locks/Condition.html
                 }
-                //println(i is Int)
                 Worker("Task ${i+1}").run() // creates and runs an instance of the Worker class with the name Task $i
                 count++ // increment so that the next thread in line can proceed
                 condition.signalAll() //  wake up all the threads that were hit with the .await() method
             }
-            finally {
-                lock.unlock() // need to unlock the lock so the next thread can execute the lambda function
+            finally { // finally garuntees that the lock will be unlocked if something goes wrong
+                access.unlock() // need to unlock the lock so the next thread can execute the lambda function. when hold count == 0 it releases the lock
             }
         }
 
@@ -71,9 +70,3 @@ fun main() {
     pTasks.forEach { it.join() }   // wait for completion before exiting
     // no index is defined so we need to use $it
     }
-
-
-
-
-
-
