51e30d9d4cba2a0f019406ffccaebbec7254fb57
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2025-11-05 19:59:57 -0600

    Refactor OpenCV loading and enhance comments
    Updated OpenCV loading method and improved comments for clarity.

--- Main.kt
+++ Main.kt
@@ -3,8 +3,6 @@ import org.opencv.core.Core
 import org.opencv.core.Mat
 import org.opencv.imgproc.Imgproc
 import org.opencv.videoio.VideoCapture
-import org.opencv.videoio.Videoio
-import org.opencv.imgcodecs.Imgcodecs
 import java.awt.Graphics
 import java.awt.image.BufferedImage
 import java.awt.image.DataBufferByte
@@ -17,57 +15,60 @@ import javax.swing.SwingUtilities
 
 fun main() {
     // Load OpenCV with bundled natives (must be first)
-    OpenCV.loadShared()
+    OpenCV.loadLocally()
 
     SwingUtilities.invokeLater {
-        val frame = JFrame("Webcam Live Feed with Filter")
-        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        val frame = JFrame("Webcam Live Feed with Filter") // makes our main frame . this isnt the video but the background
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE // when you click the red button it will exit the program
 
-        val panel = VideoPanel()
-        frame.contentPane.add(panel)
+        val panel = VideoPanel() // this is the actual video panel that will be displayed
+        frame.contentPane.add(panel) // adds the video panel onto the main frame
 
-        val removeButton = JButton("Remove Filter")
-        removeButton.addActionListener {
-            panel.setGrayscale(false)
+        val removeButton = JButton("Remove Filter") // create our remove button
+        removeButton.addActionListener { // listens for button input
+            panel.setGrayscale(false) // when button is pressed make it colored
         }
-        val applyButton = JButton("Apply Filter")
-        applyButton.addActionListener {
-            panel.setGrayscale(true)
+        val applyButton = JButton("Apply Filter") // create our apply button
+        applyButton.addActionListener { // listens for button input
+            panel.setGrayscale(true) // when button is pressed make it gray
         }
 
-        frame.add(removeButton, "South")  // Button at bottom
-        frame.add(applyButton, "North")
+        frame.add(removeButton, "South")  // places the remove button at the bottom
+        frame.add(applyButton, "North") // places the apply button at the top
 
-        frame.setSize(640, 520)  // Extra height for button
-        frame.isVisible = true
+        frame.setSize(500, 500) // sets the frame size
+        frame.isVisible = true // we need this to actually see the GUI
 
-        val cap = VideoCapture(0)
-        if (!cap.isOpened) {
+        val cap = VideoCapture(0) // this is initializing our camera , 0 is the default camera (usually the laptop camera)
+        if (!cap.isOpened) { // if the camera does not open we stop executing
             println("Error: Could not open camera")
-            return@invokeLater
+            return@invokeLater // aborts the lambda . will not move towards the executor
         }
 
-        cap.set(Videoio.CAP_PROP_FRAME_WIDTH, 640.0)
-        cap.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480.0)
+        //cap.set(Videoio.CAP_PROP_FRAME_WIDTH, 640.0)
+        //cap.set(Videoio.CAP_PROP_FRAME_HEIGHT, 480.0)
 
-        val executor = Executors.newSingleThreadExecutor()
-        executor.execute {
-            val colorMat = Mat()
-            val grayMat = Mat()
-            while (true) {
-                if (!cap.read(colorMat)) break
+        val executor = Executors.newSingleThreadExecutor() // we need a seperate thread pool from the GUI so that the background
+        // video proccesses do not interfere with the GUI . without this we wouldnt be able to click the buttons or resize the window
+        // we only need one thread since only one task is being executed (the video loop)
+        executor.execute { // starts the lambda . this will be what is executed in the background , frame by frame
+            val colorMat = Mat() // create a matrix for our colored image . multi dimensional array for pixels
+            val grayMat = Mat() // this matrix needs to be created since gray images and colored images use a different amount of channels
+            while (true) { // infinite loop since video capture is continuous
+                if (!cap.read(colorMat)) break // takes next frame
 
-                val displayMat = if (panel.isGrayscale()) {
+                val displayMat = if (panel.isGrayscale()) { //  if the user presses apply filter button it changes it to grayscale
                     Imgproc.cvtColor(colorMat, grayMat, Imgproc.COLOR_BGR2GRAY)
                     grayMat
-                } else {
+                } else { // if not keep no filter on
                     colorMat
                 }
+                Core.flip(displayMat, displayMat, 1) // mirrors the camera
 
-                panel.updateImage(displayMat)
+                panel.updateImage(displayMat) // update the panel with correct display . copies pixels from a MAT format to a BufferedImage format
             }
             cap.release()
-            colorMat.release()
+            colorMat.release() // we need to release everything upon close . its good practice and prevents memory leaks
             grayMat.release()
         }
     }
@@ -85,12 +86,12 @@ class VideoPanel : JPanel() {
     }
 
     fun updateImage(mat: Mat) {
-        currentMat = mat.clone()
+        currentMat = mat.clone() // clone the matrix for safety reasons
 
-        val width = mat.cols()
-        val height = mat.rows()
+        val width = mat.cols() // the amount of columns of pixels we have is the width of the panel
+        val height = mat.rows() // the amount of rows of pixels we have is the height of the panel
 
-        val channels = mat.channels()
+        val channels = mat.channels() // get how many channels we have (1 = grayscale 3 = color)
         val type = if (channels > 1) BufferedImage.TYPE_3BYTE_BGR else BufferedImage.TYPE_BYTE_GRAY
 
         val bufferSize = channels * width * height
