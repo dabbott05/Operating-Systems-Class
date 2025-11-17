cb3ecde29d5a8e4504aebf041f4f7b7983c5e3c7
Daniel Abbott <145143890+dabbott05@users.noreply.github.com>
2025-11-17 11:51:22 -0600

    Refactor file reading and printing functions

--- Project4/fileMNGMT.kt
+++ Project4/fileMNGMT.kt
@@ -33,7 +33,7 @@ fun appendTextFile(file : File, sourceFile : File) {
 
 }
 
-fun readTextFile(file : File) : ArrayList<Record>{
+fun readTextFile(file : File) : List<Record>{
     val records = ArrayList<Record>()
     file.bufferedReader().use { reader -> // bufferedreader to efficiently read the file . stores in reader
         reader.forEachLine { line -> // for each line in the reader
@@ -123,16 +123,7 @@ fun readJsonFile(file : File) : List<Record> {
 
 }
 
-fun printTableJSON(records: List<Record>) {
-    println("| First Name | Last Name | DOB        | Phone           | Street Address     | City           | State | Zip     |")
-    println("|------------|-----------|------------|-----------------|--------------------|----------------|-------|---------|")
-
-    records.forEach { record ->
-        println("| ${record.firstName.padEnd(10)} | ${record.lastName.padEnd(9)} | ${record.dob.padEnd(10)} | ${record.phone.padEnd(15)} | ${record.street.padEnd(22)} | ${record.city.padEnd(14)} | ${record.state.padEnd(5)} | ${record.zip.padEnd(7)} |")
-    }
-}
-
-fun printTableTXT(records: ArrayList<Record>) {
+fun printTable(records: List<Record>) {
     println("| First Name | Last Name | DOB        | Phone           | Street Address     | City           | State | Zip     |")
     println("|------------|-----------|------------|-----------------|--------------------|----------------|-------|---------|")
 
@@ -174,7 +165,7 @@ fun main() {
     appendJsonFile(fileJson, sourceFile50)
 
     println("JSON FILE START")
-    printTableJSON(readJsonFile(fileJson))
+    printTable(readJsonFile(fileJson))
     println("TEXT FILE START")
-    printTableTXT(readTextFile(fileTxt))
+    printTable(readTextFile(fileTxt))
 }
